99d1998506ce1abf183db874d9dd65c3266b6a0d
Rasmus Kuusmaa <77f315@gmail.com>
2026-08-25 08:30:06 +0300

    test(mobile): add widget test harness and app boot smoke test

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,16 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:discgolf_tracker/features/auth/presentation/login_screen.dart';
 import 'package:discgolf_tracker/main.dart';
 
 void main() {
-  testWidgets('app builds without error', (WidgetTester tester) async {
+  testWidgets('boots to the login screen when unauthenticated', (
+    WidgetTester tester,
+  ) async {
     await tester.pumpWidget(
       const ProviderScope(child: DiscGolfTrackerApp()),
     );
     await tester.pumpAndSettle();
 
-    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(find.byType(LoginScreen), findsOneWidget);
   });
 }
